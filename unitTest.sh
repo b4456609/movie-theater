7dec98daa1b96eb0e2e52d41eb0202c636f4f263
Bernie <b4456609@gmail.com>
2017-03-17 08:08:44 +0800

    unit test sh

---
+++ unitTest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+docker run -d -p 27017:27017 --name=testmongo mongo
+./gradlew clean
+./gradlew unitTest
+./gradlew clientTest
+docker kill testmongo
+docker rm -v testmongo
+
+curl -i -X POST \
+    -H "Content-Type: multipart/form-data" \
+    -F "file=@./build/mca/mpd.json" \
+    http://$MGP/api/upload
+
+./gradlew pactPublish
